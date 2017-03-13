012ac79c3fc7c2c949fc76e61db53498fa0ae68c
Mark Sellors <github@5vcc.com>
2017-03-13 13:56:44 +0000

    Initial commit of notes

---
+++ mycran-notes.R
@@ -0,0 +1,32 @@
+localCRAN <- path.expand("~/local-cran")
+dir.create(localCRAN)
+
+contribDir <- file.path(localCRAN, "src", "contrib")
+dir.create(contribDir, recursive = TRUE)
+
+rVersion <- paste(unlist(getRversion())[1:2], collapse = ".")
+
+binPaths <- list(
+  win.binary = file.path("bin/windows/contrib", rVersion),
+  mac.binary = file.path("bin/macosx/contrib", rVersion),
+  mac.binary.mavericks = file.path("bin/macosx/mavericks/contrib", rVersion),
+  mac.binary.leopard = file.path("bin/macosx/leopard/contrib", rVersion)
+)
+
+binPaths <- lapply(binPaths, function(x) file.path(localCRAN, x))
+lapply(binPaths, function(path) {
+  dir.create(path, recursive = TRUE)
+})
+
+
+sashimiDescPath <- file.path(tempdir(), "sashimi", "DESCRIPTION")
+cat("Repository: sushi", file = sashimiDescPath, append = TRUE, sep = "\n")
+
+
+
+
+
+tools::write_PACKAGES(contribDir, type = "source")
+lapply(binPaths, function(path) {
+  tools::write_PACKAGES(path)
+})
